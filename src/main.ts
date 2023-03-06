3e7d6c35645c67a9899b1a667048e4942fb4f7ca
Jackson Rodriguez <jackson.rodriguezf@gmail.com>
2023-03-06 15:31:01 -0500

    feat: adding and setting up compression
    - the idea is to decrease the size of the response body, thereby increasing the speed of a web app.

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import { Logger, LoggerErrorInterceptor } from 'nestjs-pino';
+import compression from '@fastify/compress';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
 
+  /**
+   * Strip validated (returned) object of any properties that do not use any validation decorators.
+   * Automatically transform payloads to be objects typed according to their DTO classes.
+   * **/
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -15,9 +20,14 @@ async function bootstrap() {
     }),
   );
 
+  // Enabling pino-logger
   app.useLogger(app.get(Logger));
   app.useGlobalInterceptors(new LoggerErrorInterceptor());
 
+  // Tells fastify-compress to only use gzip and deflate encodings, preferring gzip if the client supports both.
+  await app.register(compression, { encodings: ['gzip', 'deflate'] });
+
+  // The 0.0.0.0 is set to accept connections on other hosts
   await app.listen(process.env?.APP_PORT ?? 3000, '0.0.0.0');
 }
 bootstrap();
