2ae77a1f072ed1b094caf60ce2034d3e17990fbd
Jackson Rodriguez <jackson.rodriguezf@gmail.com>
2023-03-06 16:51:40 -0500

    feat: adding health check status for database
    - cleaning the application by removing unnecessary files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,13 @@
 import { Module } from '@nestjs/common';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
 // App Modules
-import { ConfigModule } from './config/config.module';
-import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from './config';
+import { DatabaseModule } from './database';
+import { HealthModule } from './health';
 
 @Module({
-  imports: [ConfigModule, DatabaseModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConfigModule, DatabaseModule, HealthModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
