4bbe4f28ab4647fae2bc32a24a74920cfd0a440e
Jackson Rodriguez <jackson.rodriguezf@gmail.com>
2023-03-06 16:11:13 -0500

    feat: adding swagger support

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import { Logger, LoggerErrorInterceptor } from 'nestjs-pino';
 import compression from '@fastify/compress';
@@ -31,8 +32,27 @@ async function bootstrap() {
   // Tells fastify-compress to only use gzip and deflate encodings, preferring gzip if the client supports both.
   await app.register(compression, { encodings: ['gzip', 'deflate'] });
 
+  // Setting up Swagger
+  const config = new DocumentBuilder()
+    .setTitle(process.env?.APP_NAME ?? 'Swagger')
+    .setDescription('Small ride-hailing service that uses Wompi for monetary transactions')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   // Initialize helmet to protect the app from some well-known web vulnerabilities
-  await app.register(helmet);
+  await app.register(helmet, {
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: [`'self'`],
+        styleSrc: [`'self'`, `'unsafe-inline'`],
+        imgSrc: [`'self'`, 'data:', 'validator.swagger.io'],
+        scriptSrc: [`'self'`, `https: 'unsafe-inline'`],
+      },
+    },
+  });
 
   // The 0.0.0.0 is set to accept connections on other hosts
   await app.listen(process.env?.APP_PORT ?? 3000, '0.0.0.0');
